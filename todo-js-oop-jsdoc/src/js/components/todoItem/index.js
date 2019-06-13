c10384067a715cc12fb977a5f7f1e1849de460ba
Alex <avataka9@gmail.com>
2019-06-13 22:11:53 +0300

    step

--- todo-js-oop-jsdoc/src/js/components/todoItem/index.js
+++ todo-js-oop-jsdoc/src/js/components/todoItem/index.js
@@ -1,65 +1,14 @@
-import {defaultView} from "./view/defaultView";
 'use strict';
 
-
-export class TodoItem {
-	constructor(config) {
-    this._nearParent = config.nearParent;
-		this._name = config.name || 'default name';
-		this._description = config.description || 'write your description';
-		this._finished = config.finished || false;
-		this._container = this.createContainer(config.name);
-		this.editText = config.editText;
-		// this.deleteSelf = this.deleteSelf.bind(this);
-	}
+/**
+ * @class
+ */
+export class Task {
 	
-	createContainer() {
-		let elem = document.createElement('div');
-		return elem;
-	}
+	/**
+	 * @constructor
+	 */
+	constructor() {
 	
-	getContainer() {
-		return this._container;
-	}
-
-	// deleteSelf() {
-	// 	this._nearParent.removeChild(this._container)
-	// }
-	
-	render() {
-		/*this._container.innerHTML = defaultView({
-			name: this._name,
-			description: this._description,
-			finished: this._finished,
-			deleteSelf: this.deleteSelf
-		});*/
-		this._container.className = `todo-item ${this._finished ? 'todo-item--finished':''} no-swipe`;
-		
-		let checkbox = document.createElement('input');
-		checkbox.type = 'checkbox';
-		checkbox.className = "todo-item__checkbox";
-		checkbox.checked = this._finished;
-		
-		let name = document.createElement('div');
-		name.className = 'todo-item__name';
-		name.textContent = this._name;
-		name.ondblclick = this.editText;
-		let description = document.createElement('div');
-		description.className = 'todo-item__description';
-		description.textContent = this._description;
-		description.ondblclick = this.editText;
-		
-		let deleteButton = document.createElement('input');
-		deleteButton.className = 'todo-item__delete';
-		deleteButton.type = 'button';
-		deleteButton.value = 'delete';
-		deleteButton.onclick = this.deleteHandler;
-
-		this._container.appendChild(checkbox);
-		this._container.appendChild(name);
-		this._container.appendChild(description);
-		this._container.appendChild(deleteButton);
-		
-		return this;
 	}
 }
--- todo-js-oop-jsdoc/src/js/components/todoList/index.js
+++ todo-js-oop-jsdoc/src/js/components/todoList/index.js
@@ -1,132 +1,14 @@
 'use strict';
 
-export class TodoList {
-  constructor(config) {
-    this._TodoItem = config.TodoItem;
-    this._nearParent = config.nearParent;
-    this.createNewTodoFromEvent = this.createNewTodoFromEvent.bind(this);
-    this.editText = this.editText.bind(this);
-    // this.deleteTodo = this.deleteTodo.bind(this);
-    this.deleteList = this.deleteList.bind(this);
-    this._container = this._createContainer(config.name);
-  };
-
-  editText({target}) {
-  	//let clone = target.cloneNode(true);
-  	//clone.ondblclick = this.editText;
-		let text = target;
-    let replaced = text;
-    let parent = target.parentElement;
-
-
-		let input = document.createElement('input');
-		input.type = 'text';
-		input.value = text.textContent;
-		input.onchange = function () {
-			text.textContent = input.value;
-			//parent.insertBefore(clone, parent.firstChild);
-			//parent.removeChild(input);
-			replaced = parent.replaceChild(text, input)
-    };
-
-		replaced = parent.replaceChild(input, text)
-    //parent.insertBefore(input, parent.firstChild);
-    //parent.removeChild(target);
+/**
+ * @class
+ */
+export class TaskList {
+	
+	/**
+	 * @constructor
+	 */
+	constructor() {
+	
 	}
-
-  deleteTodo(child,event) {
-
-    this._container.children[2].removeChild(child);
-  }
-
-  deleteList() {
-    this._nearParent.removeChild(this._container)
-	}
-
-  createNewTodoFromEvent(event) {
-    let name = event.target.firstElementChild.value;
-    let todo = this.createNewTodo(name);
-    this._container.children[2].appendChild(todo);
-    event.target.firstElementChild.value = '';
-    event.preventDefault();
-  };
-
-  createNewTodo(name, description, finished) {
-    let todo = new this._TodoItem({
-      nearParent: this._container.children[1],
-      name: name,
-      description: description,
-      finished: finished,
-			editText: this.editText
-    });
-    let todoContainer = todo.getContainer();
-    todo.deleteHandler = this.deleteTodo.bind(this, todoContainer);
-    todo.render();
-    return todoContainer;
-  };
-
-  _createContainer(name) {
-		let elem = document.createElement('div');
-		elem.className = 'todo-list';
-
-    let deleteButton = document.createElement('input');
-    deleteButton.className = 'todo-item__delete';
-    deleteButton.type = 'button';
-    deleteButton.value = 'delete';
-    deleteButton.onclick = this.deleteList;
-
-
-		let head = document.createElement('div');
-		head.className = 'todo-list__head no-swipe';
-		let title = document.createElement('div');
-		title.className = 'todo-list__title';
-		title.textContent = `${name}`;
-		title.ondblclick = this.editText;
-		head.appendChild(title);
-
-		let newTodo = document.createElement('form');
-		newTodo.className = 'new-todo';
-		let newTodoInput = document.createElement('input');
-		newTodoInput.className = 'new-todo__input';
-		newTodoInput.type = 'text';
-		newTodo.appendChild(newTodoInput);
-		let newTodoButton = document.createElement('button');
-		newTodoButton.className = 'new-todo__button';
-		newTodoButton.textContent = 'Add';
-		newTodo.appendChild(newTodoButton);
-		newTodo.action = "#";
-		newTodo.method = "POST";
-		newTodo.onsubmit = this.createNewTodoFromEvent;
-
-		head.appendChild(newTodo);
-
-		let body = document.createElement('div');
-		body.className = 'todo-list__body';
-
-		elem.appendChild(deleteButton);
-		elem.appendChild(head);
-		elem.appendChild(body);
-
-		return elem;
-	};
-
-  getContainer() {
-    return this._container;
-  };
-
-  renderTodoItemsFromJSON(json) {
-		/*body.innerHTML = json.todos.reduce((all, currentItem) => all + `${
-		 new this._TodoItem().render(currentItem)
-		 }`, '');*/
-
-    json.forEach((currentItem) => this._container.children[2].appendChild(this.createNewTodo(currentItem.name, currentItem.description, currentItem.finished)));
-
-    return this;
-
-
-
-
-
-  };
-
 }
